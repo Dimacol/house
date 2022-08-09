375d5ca36e98c36e7843e5ff809d6985afd52c75
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-09 13:13:21 +0300

    все задачи

---
+++ task001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA < numberB) 
+{
+    Console.WriteLine("max = " + numberB); 
+}
+else if (numberB < numberA) 
+{ Console.WriteLine("max = " + numberA); 
+}
---
+++ task002/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+if (numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine("max =" + numberA);
+} 
+else if (numberB > numberC && numberB > numberA)
+{
+    Console.WriteLine("max =" + numberB);}
+else if (numberC > numberA && numberC > numberB){Console.WriteLine("max =" + numberC);
+}
+else if (numberA == numberB || numberA == numberC)
+{
+    Console.WriteLine("max =" + numberA);
+}
+else if (numberB == numberC)
+{
+    Console.WriteLine("max = " + numberB);
+}
