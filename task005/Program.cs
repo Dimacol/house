d33b279b1863dfcdba899e3c3486a32957f26446
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-11 11:03:05 +0300

    задача5

---
+++ task005/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine());
+
+// int a = number % 100;
+// int b = a / 10;
+// Console.WriteLine("Second number: " + b);
+
+string strnumber = number.ToString();
+Console.WriteLine("Второе число : " + strnumber[1]);
