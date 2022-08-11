d2b9cdd0bdbaf787c3266147e5578eee9abc258d
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-11 11:10:35 +0300

    задача6

---
+++ task006/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string strnumber  = number .ToString();
+if (strnumber .Length > 2)
+{
+    Console.WriteLine("третья цифра: " + strnumber [2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
