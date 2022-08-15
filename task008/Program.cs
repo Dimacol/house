078eded0f4dbaefedf56f3472223b8f0453ea82c
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-15 12:03:58 +0300

    задача полиндром

---
+++ task008/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+void Palindrom(string x)
+{    
+    if (x[0] == x[4] && x[1] == x[3])
+        Console.WriteLine("Это полиндпром");
+    else
+    {
+        Console.WriteLine("это не полидром");
+    }
+    return;
+}
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if (number.Length == 5)
+{
+    Palindrom(number);
+}
+else Console.WriteLine("попробуй еще раз");
