dfa6fec6de941e2148bca8d0f96f446707f8e3df
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-21 15:31:54 +0300

    Массив из 8 чисел

---
+++ task013/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
+
+void FillArray(int[] array)
+{
+    int lenght = array.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int lenght = array.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
