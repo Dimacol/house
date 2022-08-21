67ce8c01b4e42e13b52037e9b3d428c1f250a7a9
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-21 15:22:27 +0300

    a в степени b

---
+++ task011/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+int ApowB(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+
+int B = int.Parse(Console.ReadLine());
+int power = ApowB(A, B);
+Console.WriteLine("А в степени В: " + power);
