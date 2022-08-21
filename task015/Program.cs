1218e96a0ef3dafb3433398a4485569271e47d0a
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-21 15:44:15 +0300

    сумма элементов, стоящих на нечётных позициях

---
+++ task015/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] FillArray(int[] array, int min = -99, int max = 100)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    Console.WriteLine(string.Join(", ", array));
+    return array;
+}
+
+int SumOddNumbers(int [] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ( i % 2 != 0)  {summ += array[i];} 
+    }
+    Console.WriteLine("сумма элементов, стоящих на нечётных позициях: " + summ);
+    return summ;
+}
+int [] array = new int[10];
+SumOddNumbers(FillArray(array)); 
