3b0a30c47f8d7c43399e2cb0006a01676a17bb16
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-21 15:28:49 +0300

    Сумма цифр в числе

---
+++ task012/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+
+int summ(int number)
+{
+    string num = Convert.ToString(number);
+    int sum = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        int temp = number % 10;
+        sum += temp;
+        number /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число:");
+int A = int.Parse(Console.ReadLine());
+if (A <= 0) { Console.WriteLine("Ошибка"); }
+else Console.WriteLine("Сумма цифр в числе:" + summ(A));
