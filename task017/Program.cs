395d2df761f57862ae5c70b00497f767c04eb638
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-26 15:06:34 +0300

    Числа больше нуля

---
+++ task017/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел :");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+void NumbersBolZero(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+
+        if (numbers[i] > 0)
+        {
+            count++;
+        }
+
+    }
+    Console.WriteLine("Чисел больше 0: " + count);
+}
+
+NumbersBolZero(numbers);
