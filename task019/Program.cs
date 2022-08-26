47af2dc10a3cf4ab74a5d1776fce706e4e2520e7
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-26 15:34:04 +0300

    Сумма элементов диагонали

---
+++ task019/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив со случайными числами от -10 до 10. 
+// Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.)
+
+// Например, задан массив:
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+void PrintArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().Next(-10, 11);
+        }
+    }
+}
+
+int summ (int [,] table)
+{
+    int summ = 0;
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            if (i == j) summ += table[i, j];
+        }
+    }
+    Console.WriteLine($"Cумма элементов, находящихся на главной диагонали: {summ}");
+    return summ;
+}
+
+int[,] array = new int[5,5];
+FillArray(array);
+PrintArray(array);
+summ(array); 
