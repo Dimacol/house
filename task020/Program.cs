9a831b193148e6b42c8f03c7111c1b1998c53a76
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-08-31 18:13:01 +0300

    Задайте двумерный массив размером mхn

---
+++ task020/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+void PrintArray(double[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F2}", mtrx[i, j], " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] mtrx)
+{
+    Random random = new Random();
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            mtrx[i, j] = random.NextDouble() + random.Next(-9, 10);
+        }
+    }
+}
+Console.WriteLine("Введите размер массива");
+Console.Write("Введите количество строк: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int m = int.Parse(Console.ReadLine());
+double[,] array = new double[n, m];
+
+FillArray(array);
+PrintArray(array);
