0d91d4cb0ae6d243a822365580d860d55a11d7d3
Пользователь-2 SGn <sgn.user2@SGn.local>
2022-09-05 11:03:44 +0300

    Рекурсия натуральные числа в промежутке от M до N

---
+++ task028/Program.cs
@@ -0,0 +1,18 @@
+﻿int RangeMtoN(int m, int n)
+{
+    if (m == n) return n;
+    else
+        Console.Write($"{m}, ");
+    return RangeMtoN(m + 1, n);
+}
+
+Console.WriteLine("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (M > N) Console.WriteLine("Ошибка M<N!");
+else
+{
+    int result = RangeMtoN(M, N);
+   Console.Write($"{result} ");
+}
